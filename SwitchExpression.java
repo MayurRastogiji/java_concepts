bd48e412282a4070bc51befb3c0ff9a14995fec4
Mayur Rastogi <mayur.rastogi.cseaiml.2022@miet.ac.in>
2024-08-17 11:22:19 +0530

    update a file

--- SwitchExpression.java
+++ SwitchExpression.java
@@ -1,16 +1,23 @@
-public enum day{
-    Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday;
+public enum day {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday;
 }
-class SwitchExpression{
+
+class SwitchExpression {
     public static void main(String[] args) {
         day d = day.Sunday;
-        int x = switch (d){
-            case Monday,Friday,Sunday -> 10;
-            case Tuesday,Saturday -> 15;
-            case Wednesday,Thursday -> 12;
+        // we can use yield keyword after arrow it will work same as break keyword work in normal switch case
+        int x = switch (d) {
+            case Monday, Friday, Sunday -> 10;
+            case Tuesday, Saturday -> 15;
+            case Wednesday, Thursday -> 12;
             default -> throw new IllegalStateException("invalid data");
         };
         System.out.println(x);
     }
-    
 }
