045292a70e1ce249710bbf1d70081516be904984
Mayur Rastogi <mayur.rastogi.cseaiml.2022@miet.ac.in>
2024-12-09 10:09:04 +0530

    update
    Constructor.java
    Static_Keyword.java

--- Constructor.java
+++ Constructor.java
@@ -4,9 +4,15 @@
  * Constructor have the same name as of the class.
  * It does not have any return type means we are not able to use void keyword with it.
  * We can do method overloading in constructor
+ * static block only runs once when object is created it doesn't depends on number of object.
+ * If we just want to load a class without creating an object of it , We can use "class.forname(Pass your function name)" function
  */
 
  class box{
+    static String name;                 //Static keyword
+    static {
+        name = "mayur";                 //To initialize static String we use static keyword, in place of constructor.
+    }
     int x,y,z;
     box (){
         x = 1;
@@ -23,10 +29,13 @@
     }
  }
  class Constructor{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         box b1 = new box();
         b1.volume();
         box b2 = new box(5,4,9);
         b2.volume();
+        System.err.println("your name :" + box.name);
+
+        Class.forName("box");           //If we only run this line of code it will not create an object , only loads a class., We normally do not use it.
     }
  }
--- Static_Keyword.java
+++ Static_Keyword.java
@@ -1,9 +1,10 @@
 // Implementation of static Keyword
 /* 
- * Static Keyword is use when we want to use same variable throughout stoarge of that variable will be alloted by just declaring it, no other memory will be allocated by making object 
- * We can only access static variables in static methods, if we try to access normal variable it will through error.
+ * Static Keyword is use when we want to use same variable throughout storage of that variable will be alloted by just declaring it, no other memory will be allocated by making object 
+ * In static methods we can only access static variables, if we try to access normal variable it will through error.
  * While we can access both static and normal variable in normal methods. 
  * But after making object of it we can use it easily. 
+ * we can use static methods without making its object.
  
  */
 
@@ -11,6 +12,15 @@ class A {
 
     int i = 5;
     static int j = 5;
+    String name = "";
+
+    public static void hello(){
+        System.out.println("Hello bro.");
+    }
+
+    public static void hello(A obj){                        //accepting object to get access of non-static or Instance variable. 
+        System.out.println("Hello "+ obj.name);
+    }
 }
 
 class Static_Keyword {
@@ -19,18 +29,21 @@ class Static_Keyword {
         System.out.println("We can use static variable without making there object");
         System.out.println(A.j);
         A a1 = new A();
+        a1.name = "mayur";
         A a2 = new A();
         System.out.println("Before increment");
         System.out.println(a1.i);
-        System.out.println(a1.j);
+        System.out.println(A.j);
         System.out.println("After increment");
         System.out.println(++a1.i);
-        System.out.println(++a1.j);
+        System.out.println(++A.j);
         // It will give error
         // System.out.println(A.i);
 
         System.out.println(a2.i);
-        System.out.println(a2.j);
+        System.out.println(A.j);
+        A.hello();                  //Accessing static method directly without creating its object
+        A.hello(a1);                //Passing object to the static method
 
     }
 }
